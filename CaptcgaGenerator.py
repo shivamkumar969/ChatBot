b4087c3bfc046361941e79fb859264015256f80d
Shivam Kumar <shivamkumar101202@gmail.com>
2025-08-22 20:39:51 +0530

    CaptcgaGenerator.py

--- CaptcgaGenerator.py
+++ CaptcgaGenerator.py
@@ -4,8 +4,9 @@ from flask import session
 import random
 import string
 def get_random_code():
-    all_chars=string.ascii_letters+string.digits
-    length=random.randint(5,7)
+    # all_chars=string.ascii_letters+string.digits # this line generate captcha mix of alphanumeric
+    all_chars=string.digits
+    length=random.randint(3,5)
     random_code="".join(random.sample(all_chars,length))
     return random_code
 
