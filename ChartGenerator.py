40fa3e1f9a4c7c97f79df8e9dc22883e8a236c81
Shivam Kumar <shivamkumar101202@gmail.com>
2026-03-21 00:09:55 +0530

    Update ChartGenerator.py

--- ChartGenerator.py
+++ ChartGenerator.py
@@ -4,5 +4,8 @@ def create_chart():
  fpath="static/knowledge_base"
                  kn_df=pd.read_excel(fpath+"CB_knowledgeBase.xlsx") 
  df=(kn_df.groupby(['Category'])['Frequency'].sum().sort_values(ascending=False))
- #print(df)                plt.figure(figsize=(6,5),facecolor='pink')     df.plot(kind="pie",ylabel="",autopct='%1.1f%%')
+
+ #print(df)   
+             plt.figure(figsize=(6,5),facecolor='pink')   
+   df.plot(kind="pie",ylabel="",autopct='%1.1f%%')
 plt.savefig("static/Chart/frequency_pie.png")
