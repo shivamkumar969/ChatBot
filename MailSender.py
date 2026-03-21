cdbef7e604ae54ee6080ba4a23372cd067e984bf
Shivam Kumar <shivamkumar101202@gmail.com>
2026-03-21 10:28:47 +0530

    Refactor email sending function to use parameters

--- MailSender.py
+++ MailSender.py
@@ -1,16 +1,14 @@
-from flask_mail import Mail,Message
-def send_my_email(app_obj,sendto,subject,message):
+from flask_mail import Message
+
+def send_my_email(mail, sendto, subject, message):
     try:
-        app_obj.config['MAIL_SERVER']='smtp.gmail.com'
-        app_obj.config['MAIL_PORT'] = 587
-        app_obj.config['MAIL_USE_TLS']=True
-        app_obj.config['MAIL_USERNAME']='shivamji101202@gmail.com'
-        app_obj.config['MAIL_PASSWORD']='vezp ahya xxpo trgo'
-        app_obj.config['MAIL_DEFAULT_SENDER']='shivamji101202@gmail.com'
-        mail=Mail(app_obj)
-        msg=Message(subject=subject,recipients=[sendto])
-        msg.html=message
+        msg = Message(
+            subject=subject,
+            recipients=[sendto]
+        )
+        msg.html = message
         mail.send(msg)
         return True
-    except:
+    except Exception as e:
+        print("Mail Error:", e)
         return False
